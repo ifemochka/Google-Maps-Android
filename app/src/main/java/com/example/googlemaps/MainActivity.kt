ba9cf6a2fc26771c791e25a3cdaa1287d3806b2a
ifemochka <mslalena@mail.ru>
2025-07-05 23:10:26 +0300

    googlemaps

--- app/src/main/java/com/example/googlemaps/MainActivity.kt
+++ app/src/main/java/com/example/googlemaps/MainActivity.kt
@@ -106,17 +106,4 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-data class OrderItem(
-    val type: String,
-    val number: Int,
-    val address: String,
-    val name: String,
-    val orderTime: String,
-    val volume: Double,
-    val weight: Double,
-    val price: Double
-)
 
-object Data {
-    var orders : List<OrderItem>? = emptyList()
-}
